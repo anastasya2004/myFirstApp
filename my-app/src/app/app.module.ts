7211ab3a7cdbfc2af1c8de086716f9825807bab8
anastasya2004 <anastasya2004@bk.ru>
2020-03-24 21:02:23 +0700

    мы добавили header

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyHeaderComponent } from './my-header/my-header.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      MyHeaderComponent,
+      MyHeaderComponent,
+      MyHeaderComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
